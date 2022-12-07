c51bd132f815a6d043a172241e80942479d36e7e
Sukyung Cho <doradora523@naver.com>
2022-12-07 21:50:25 +0900

    feat: Create the Login Form

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          value={email}
+          required
+          onChange={onChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          required
+          onChange={onChange}
+        />
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <button type="">Continue with Google</button>
+        <button type="">Continue with Github</button>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
